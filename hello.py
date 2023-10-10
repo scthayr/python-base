5a7f805948ffb9f4262beb888d1a725a3859d709
scthayr <scthayprogrammer@outlook.com>
2023-10-10 14:11:43 -0300

    Implemented hello world multi languages.

--- hello.py
+++ hello.py
@@ -1,1 +1,40 @@
-print("Hello, World!")
+
+"""
+Hello World Multi Languages.
+Depending on the language configured in 
+the environment the program displays the 
+corresponding message.
+
+How to use:
+
+Have the LANG variable properly configured ex:
+
+   export LANG=pt_BR
+
+Execution:
+
+   python hello.py
+   or
+   ./hello.py
+"""
+
+
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"    
+
+print(msg)
+
+
